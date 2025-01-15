20534992ddab2b26b1f2ef745d5c462725516a42
NJ <naveenkumar.jangir@cognizant.com>
2025-01-15 16:13:21 +0530

    updated villages and gotras for women

--- src/frontend/filter/Filter.js
+++ src/frontend/filter/Filter.js
@@ -104,23 +104,25 @@ const Filter = ({ state, dispatch, getHindiText, getHindiNumbers }) => {
     state.members[0].children?.map(member => traverseDeadAndUnmarriedFemales(member))
     return deadAndUnmarriedFemales;
   }
-  // count villages for men (works only with 1 wife)
-  const villagesForMales = [];
+  // count villages for men
+  let villagesForMales = [];
   const traverseVillagesForMales = (member) => {
     member.gender === 'M' && member.wives?.length && member.wives[0].village?.length && villagesForMales.push(member.wives[0].village);
     member.gender === 'M' && member.children?.map((child) => traverseVillagesForMales(child));
   };
   const getVillagesForMales = () => {
+    villagesForMales = [];
     state.members.forEach(member => traverseVillagesForMales(member));
     return villagesForMales.filter((item, index, self) => self.indexOf(item) === index).sort();
   };
   // count gotras for men
-  const gotrasForMales = [];
+  let gotrasForMales = [];
   const traverseGotrasForMales = (member) => {
     member.gender === 'M' && member.wives?.length && member.wives[0].gotra?.length && gotrasForMales.push(member.wives[0].gotra);
     member.gender === 'M' && member.children?.map(child => traverseGotrasForMales(child));
   };
   const getGotrasForMales = () => {
+    gotrasForMales = [];
     state.members.forEach(member => traverseGotrasForMales(member));
     return gotrasForMales.filter((item, index, self) => self.indexOf(item) === index).sort();
   };
@@ -148,6 +150,50 @@ const Filter = ({ state, dispatch, getHindiText, getHindiNumbers }) => {
     state.members[0].gender === 'M' && state.members[0].children?.forEach(member => traverseForMaleMembersWithGotra(member, gotra));
     return gotraForMalesCountForDropdown;
   };
+  // count villages for women
+  let villagesForFemales = [];
+  const traverseVillagesForFemales = (member) => {
+    member.gender === 'F' && member.village !== undefined && villagesForFemales.push(member.village);
+    member.children?.map(child => traverseVillagesForFemales(child));
+  };
+  const getVillagesForFemales = () => {
+    villagesForFemales = [];
+    state.members.forEach(member => traverseVillagesForFemales(member));
+    return villagesForFemales.filter((item, index, self) => self.indexOf(item) === index).sort();
+  };
+  // count gotras for women
+  let gotrasForFemales = [];
+  const traverseGotrasForFemales = (member) => {
+    member.gender === 'F' && member.gotra !== undefined && gotrasForFemales.push(member.gotra);
+    member.children?.map(child => traverseGotrasForFemales(child));
+  };
+  const getGotrasForFemales = () => {
+    gotrasForFemales = [];
+    state.members.forEach(member => traverseGotrasForFemales(member));
+    return gotrasForFemales.filter((item, index, self) => self.indexOf(item) === index).sort();
+  };
+  // count villages for for village dropdown
+  let villagesForFemalesCountForDropdown = 0;
+  const traverseForFemaleMembersWithVillages = (member, village) => {
+    member.gender === 'F' && member.village !== undefined && member.village === village && villagesForFemalesCountForDropdown++;
+    member.children?.forEach(member => traverseForFemaleMembersWithVillages(member, village));
+  };
+  const getFemaleMembersWithVillages = (village) => {
+    villagesForFemalesCountForDropdown = 0;
+    state.members[0].children?.forEach(member => traverseForFemaleMembersWithVillages(member, village));
+    return villagesForFemalesCountForDropdown;
+  };
+  // count gotras for for gotra dropdown
+  let gotraForFemalesCountForDropdown = 0;
+  const traverseForFemaleMembersWithGotra = (member, gotra) => {
+    member.gender === 'F' && member.gotra !== undefined && member.gotra === gotra && gotraForFemalesCountForDropdown++;
+    member.children?.forEach(member => traverseForFemaleMembersWithGotra(member, gotra));
+  };
+  const getFemaleMembersWithGotras = (gotra) => {
+    gotraForFemalesCountForDropdown = 0;
+    state.members[0].children?.forEach(member => traverseForFemaleMembersWithGotra(member, gotra));
+    return gotraForFemalesCountForDropdown;
+  };
   return (
     <div className='filter'>
       <fieldset className='filter-men'>
@@ -174,7 +220,8 @@ const Filter = ({ state, dispatch, getHindiText, getHindiNumbers }) => {
       <fieldset className='filter-women'>
         <legend>{state.user.language ? 'Women' : 'महिलाएं'}</legend>
         <select name='village' value={women.village} onChange={(e) => setWomen({ ...women, [e.target.name]: e.target.value })}>
-          <option value=''>{state.user.language ? 'village' : 'ससुराल'}</option>
+          <option value=''>{state.user.language ? `village (${getVillagesForFemales().length})` : `ससुराल (${getHindiNumbers(getVillagesForFemales().length.toString())})`}</option>
+          {getVillagesForFemales().map((village, i) => <option key={i} value={village}>{state.user.language ? `${village} (${getFemaleMembersWithVillages(village)})` : `${getHindiText(village, 'village')} (${getHindiNumbers(getFemaleMembersWithVillages(village).toString())})`}</option>)}
         </select>
         <div>
           <span>{state.user.language ? 
@@ -187,7 +234,8 @@ const Filter = ({ state, dispatch, getHindiText, getHindiNumbers }) => {
           }</span>
         </div>
         <select name='gotra' value={women.gotra} onChange={(e) => setWomen({ ...women, [e.target.name]: e.target.value })}>
-          <option value=''>{state.user.language ? 'gotra' : 'गोत्र'}</option>
+          <option value=''>{state.user.language ? `gotra (${getGotrasForFemales().length})` : `गोत्र (${getHindiNumbers(getGotrasForFemales().length.toString())})`}</option>
+          {getGotrasForFemales().map((gotra, i) => <option key={i} value={gotra}>{state.user.language ? `${gotra} (${getFemaleMembersWithGotras(gotra)})` : `${getHindiText(gotra, 'gotra')} (${getHindiNumbers(getFemaleMembersWithGotras(gotra).toString())})`}</option>)}
         </select>
       </fieldset>
     </div>
