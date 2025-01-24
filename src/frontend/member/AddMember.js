0a6d9e3f6d747987112494f8a1e2a9b1fbc45467
NJ <naveenkumar.jangir@cognizant.com>
2025-01-24 23:48:09 +0530

    issue fixed for email and phone

--- src/frontend/member/AddMember.js
+++ src/frontend/member/AddMember.js
@@ -7,8 +7,8 @@ const AddMember = ({state, dispatch}) => {
   const [type, setType] = useState('');
   const [newMember, setNewMember] = useState({
     name: '',
-    mobile: [],
-    email: [],
+    mobile: '',
+    email: '',
     children: [],
     wives: [],
     dob: '',
@@ -26,24 +26,24 @@ const AddMember = ({state, dispatch}) => {
           name: newMember.name,
           children: [],
           wives: [],
-          mobile: newMember.mobile,
-          dob: newMember.dob !== '' ? `${newMember.dob.split('-')[2]} ${months[Number(newMember.dob.split('-')[1]) - 1]} ${newMember.dob.split('-')[0]}` : '',
+          mobile: newMember.mobile !== '' ? [newMember.mobile] : [],
+          dob: newMember.dob !== '' ? newMember.dob.split('-')[2] + ' ' + months[parseInt(newMember.dob.split('-')[1]) - 1] + ' ' + newMember.dob.split('-')[0] : '',
           isAlive: newMember.isAlive === 'alive' ? true : false,
           gender: 'M',
           village: newMember.village,
-          email: newMember.email,
+          email: newMember.email !== '' ? [newMember.email] : [],
           isCollapsed: false,
         }
       } else {
         person = {
           id: state.member ? ((state.member.id * 10) + (state.member.children.length + 1)) : 0,
           name: newMember.name,
-          mobile: newMember.mobile,
-          dob: newMember.dob !== '' ? `${newMember.dob.split('-')[2]} ${months[Number(newMember.dob.split('-')[1]) - 1]} ${newMember.dob.split('-')[0]}` : '',
+          mobile: newMember.mobile !== '' ? [newMember.mobile] : [],
+          dob: newMember.dob !== '' ? newMember.dob.split('-')[2] + ' ' + months[parseInt(newMember.dob.split('-')[1]) - 1] + ' ' + newMember.dob.split('-')[0] : '',
           isAlive: newMember.isAlive === 'alive' ? true : false,
           gender: 'F',
           village: newMember.village,
-          email: newMember.email  
+          email: newMember.email !== '' ? [newMember.email] : [],
         }   
       }
       const response = await fetch('http://115.117.107.101:27001/addNewMember', {
@@ -52,59 +52,32 @@ const AddMember = ({state, dispatch}) => {
       body: JSON.stringify({ member: state.member, newMember: person, type: type, village: state.village })})
       const data = await response.json();
       if (data.result === 'success') {
-        dispatch({
-          type: 'addMember', 
-          member: {
-            id: state.member ? ((state.member.id * 10) + (state.member.children.length + 1)) : 0,
-            name: newMember.name,
-            children: [],
-            wives: [],
-            mobile: newMember.mobile,
-            dob: newMember.dob !== '' ? `${newMember.dob.split('-')[2]} ${months[Number(newMember.dob.split('-')[1]) - 1]} ${newMember.dob.split('-')[0]}` : '',
-            isAlive: newMember.isAlive === 'alive' ? true : false,
-            gender: newMember.gender,
-            village: newMember.village,
-            email: newMember.email,
-            isCollapsed: false,
-          }, 
-          memberType: type
-        });
+        dispatch({type: 'addMember', member: person, memberType: type});
       }
     } else if (type === 'wife') {
-      const response = await fetch('http://115.117.107.101:27001/addNewMember', {
-      method: 'post',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ member: state.member, newMember: {
+      const person = {
         id: state.member ? (state.member.id * 10) : 0,
         name: newMember.name,
-        dob: newMember.dob,
+        dob: newMember.dob !== '' ? newMember.dob.split('-')[2] + ' ' + months[parseInt(newMember.dob.split('-')[1]) - 1] + ' ' + newMember.dob.split('-')[0] : '',
         isAlive: newMember.isAlive === 'alive' ? true : false,
         gender: 'F',
         village: newMember.village,
         gotra: newMember.gotra,
-      }, type: type, village: state.village })})
+      }
+      const response = await fetch('http://115.117.107.101:27001/addNewMember', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ member: state.member, newMember: person, type: type, village: state.village })})
       const data = await response.json();
       if (data.result === 'success') {
-        dispatch({
-          type: 'addMember', 
-          member: {
-            id: state.member ? (state.member.id * 10) : 0,
-            name: newMember.name,
-            dob: newMember.dob,
-            isAlive: newMember.isAlive === 'alive' ? true : false,
-            gender: 'F',
-            village: newMember.village,
-            gotra: newMember.gotra,
-          }, 
-          memberType: type
-        });      
+        dispatch({type: 'addMember', member: person, memberType: type});      
       }
     }
     setType('');
     setNewMember({
       name: '',
-      mobile: [],
-      email: [],
+      mobile: '',
+      email: '',
       children: [],
       wives: [],
       dob: '',
@@ -153,7 +126,7 @@ const AddMember = ({state, dispatch}) => {
         <input disabled={type === ''} type='text' name='village' value={newMember.village} onChange={(e) => setNewMember({...newMember, [e.target.name]: e.target.value})} placeholder='village' />
         {type === 'wife' ? <input type='text' name='gotra' value={newMember.gotra} onChange={(e) => setNewMember({...newMember, [e.target.name]: e.target.value})} placeholder='gotra' /> : ''}
         <input disabled={type === ''} type='email' name='email' value={newMember.email} onChange={(e) => setNewMember({...newMember, [e.target.name]: e.target.value})} placeholder='email' />
-        <button disabled={type === '' || newMember.name === '' || newMember.gender === '' || newMember.isAlive === ''} onClick={() => handleAddMember()}>Add</button>
+        <button disabled={type === ''} onClick={() => handleAddMember()}>Add</button>
       </div>
     </div>
   );
