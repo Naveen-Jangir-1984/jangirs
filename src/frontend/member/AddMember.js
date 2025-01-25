598d31d99bb5c0ee90770216a0ecb949507f16fc
NJ <monu.bits@gmail.com>
2025-01-25 07:30:23 +0530

    fixed dob issue

--- src/frontend/member/AddMember.js
+++ src/frontend/member/AddMember.js
@@ -3,7 +3,9 @@ import CloseIcon from '../../images/close.png';
 import './AddMember.css';
 
 const AddMember = ({state, dispatch}) => {
+  const dates = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 23, 24, 25, 26, 27,28, 29, 30, 31];
   const months = ['Januray', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const years = [2024, 2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2014, 2013, 3012, 2011, 2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002, 2001, 2000, 1999, 1998, 1997, 1996, 1995, 1994, 1993, 1992, 1991, 1990]
   const [type, setType] = useState('');
   const [newMember, setNewMember] = useState({
     name: '',
@@ -11,7 +13,9 @@ const AddMember = ({state, dispatch}) => {
     email: '',
     children: [],
     wives: [],
-    dob: '',
+    date: '',
+    month: '',
+    year: '',
     isAlive: 'alive',
     gender: 'M',
     village: '',
@@ -27,7 +31,7 @@ const AddMember = ({state, dispatch}) => {
           children: [],
           wives: [],
           mobile: newMember.mobile !== '' ? [newMember.mobile] : [],
-          dob: newMember.dob !== '' ? newMember.dob.split('-')[2] + ' ' + months[parseInt(newMember.dob.split('-')[1]) - 1] + ' ' + newMember.dob.split('-')[0] : '',
+          dob: newMember.date !== '' && newMember.month !== '' && newMember.year !== '' ? newMember.date + ' ' + newMember.month + ' ' + newMember.year : '',
           isAlive: newMember.isAlive === 'alive' ? true : false,
           gender: 'M',
           village: newMember.village,
@@ -39,7 +43,7 @@ const AddMember = ({state, dispatch}) => {
           id: state.member ? ((state.member.id * 10) + (state.member.children.length + 1)) : 0,
           name: newMember.name,
           mobile: newMember.mobile !== '' ? [newMember.mobile] : [],
-          dob: newMember.dob !== '' ? newMember.dob.split('-')[2] + ' ' + months[parseInt(newMember.dob.split('-')[1]) - 1] + ' ' + newMember.dob.split('-')[0] : '',
+          dob: newMember.date !== '' && newMember.month !== '' && newMember.year !== '' ? newMember.date + ' ' + newMember.month + ' ' + newMember.year : '',
           isAlive: newMember.isAlive === 'alive' ? true : false,
           gender: 'F',
           village: newMember.village,
@@ -58,7 +62,7 @@ const AddMember = ({state, dispatch}) => {
       const person = {
         id: state.member ? (state.member.id * 10) : 0,
         name: newMember.name,
-        dob: newMember.dob !== '' ? newMember.dob.split('-')[2] + ' ' + months[parseInt(newMember.dob.split('-')[1]) - 1] + ' ' + newMember.dob.split('-')[0] : '',
+        dob: newMember.date !== '' && newMember.month !== '' && newMember.year !== '' ? newMember.date + ' ' + newMember.month + ' ' + newMember.year : '',
         isAlive: newMember.isAlive === 'alive' ? true : false,
         gender: 'F',
         village: newMember.village,
@@ -80,7 +84,9 @@ const AddMember = ({state, dispatch}) => {
       email: '',
       children: [],
       wives: [],
-      dob: '',
+      date: '',
+      month: '',
+      year: '',
       isAlive: 'alive',
       gender: 'M',
       village: '',
@@ -96,7 +102,9 @@ const AddMember = ({state, dispatch}) => {
       email: [],
       children: [],
       wives: [],
-      dob: '',
+      date: '',
+      month: '',
+      year: '',
       isAlive: true,
       gender: 'M',
       village: '',
@@ -114,7 +122,20 @@ const AddMember = ({state, dispatch}) => {
         </select>
         <input disabled={type === ''} type='text' name='name' value={newMember.name} onChange={(e) => setNewMember({...newMember, [e.target.name]: e.target.value})} placeholder='name' />
         <input disabled={type === ''} type='text' name='mobile' value={newMember.mobile} onChange={(e) => setNewMember({...newMember, [e.target.name]: e.target.value})} placeholder='mobile' maxLength='10' />
-        <input disabled={type === ''} type='date' name='dob' value={newMember.dob} onChange={(e) => setNewMember({...newMember, [e.target.name]: e.target.value})} />
+        <div className='dob'>
+          <select disabled={type === ''} name='date' value={newMember.date} onChange={(e) => setNewMember({...newMember, [e.target.name]: e.target.value})}>
+            <option value=''>date</option>
+            {dates.map((date, i) => <option key={i} value={date}>{date}</option>)}
+          </select>
+          <select disabled={type === ''} name='month' value={newMember.month} onChange={(e) => setNewMember({...newMember, [e.target.name]: e.target.value})}>
+            <option value=''>month</option>
+            {months.map((month, i) => <option key={i} value={month}>{month}</option>)}
+          </select>
+          <select disabled={type === ''} name='year' value={newMember.year} onChange={(e) => setNewMember({...newMember, [e.target.name]: e.target.value})}>
+            <option value=''>year</option>
+            {years.map((year, i) => <option key={i} value={year}>{year}</option>)}
+          </select>
+        </div>
         <select disabled={type === '' || type === 'wife'} name='gender' value={type === 'wife' ? 'F' : newMember.gender} onChange={(e) => setNewMember({...newMember, [e.target.name]: e.target.value})}>
           <option value='M'>Male</option>
           <option value='F'>Female</option>
