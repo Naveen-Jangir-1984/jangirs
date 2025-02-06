505a243616bd4429ef2feaef3f33642263961ed5
NJ <naveenkumar.jangir@cognizant.com>
2025-02-06 19:56:46 +0530

    reverted backdrop

--- src/frontend/header/Header.js
+++ src/frontend/header/Header.js
@@ -15,14 +15,12 @@ const Header = ({ state, dispatch, getHindiText }) => {
   return (
     <div className='header'>
       <button className='toggle-language' onClick={() => dispatch({type: 'language', flag: !state.user.language})}>{state.user.language ? getHindiText('Hindi') : 'English'}</button>
-      <select style={{visibility: state.isMemberDisplayOpen || state.isMemberAddOpen || state.isMemberEditOpen ? 'hidden' : 'visible'}} value={state.village} onChange={(e) => dispatch({type: 'village', village: e.target.value})}>
+      <select value={state.village} onChange={(e) => dispatch({type: 'village', village: e.target.value})}>
         {state.villages.map((village, i) => <option key={i} value={village}>{state.user.language ? village.replace(village.charAt(0), village.charAt(0).toUpperCase()) : getHindiText(village.replace(village.charAt(0), village.charAt(0).toUpperCase()), 'village')}</option>)}
       </select>
-      {state.user.role === 'admin' ? <img className='icons' style={{visibility: state.isMemberDisplayOpen || state.isMemberAddOpen || state.isMemberEditOpen ? 'hidden' : 'visible'}} src={UserEditIcon} alt='editUser' onClick={() => dispatch({type: 'openUserEdit'})} /> : ''}
-      <img className='signout' style={{visibility: state.isMemberDisplayOpen || state.isMemberAddOpen || state.isMemberEditOpen ? 'hidden' : 'visible'}} src={SignOutIcon} alt='signout' onClick={() => handleSignOut()} />
-      <button 
-        style={{visibility: state.isMemberDisplayOpen || state.isMemberAddOpen || state.isMemberEditOpen ? 'hidden' : 'visible'}}
-        onClick={() => {
+      {state.user.role === 'admin' ? <img className='icons' src={UserEditIcon} alt='editUser' onClick={() => dispatch({type: 'openUserEdit'})} /> : ''}
+      <img className='signout' src={SignOutIcon} alt='signout' onClick={() => handleSignOut()} />
+      <button onClick={() => {
         setCollapsed(!collapsed)
         dispatch({type: 'toggle-all', flag: collapsed})
       }}>{collapsed ? (state.user.language ? 'close' : 'बंद करे') : (state.user.language ? 'Open' : 'खोलें')}</button>
